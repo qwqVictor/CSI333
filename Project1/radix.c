bc519a7113ef9b280eb28dd047520c677be6f38e
qwqVictor <i@qwq.ren>
2022-09-19 15:05:43 +0800

    feat(proj1): buffer length calculation

--- Project1/radix.c
+++ Project1/radix.c
@@ -1,17 +1,22 @@
 #include "radix.h"
 
 const char RADIX_TABLE[17] = "0123456789ABCDEF";
+
+int calc_rlen(int value, const int radix) {
+    return (value ? (ceil(log2f(value < 0 ? -value : value) / log2f(radix))
+                     + (value < 0 ? 1 : 0)) : 1) + 1;
+}
+
 int radix_conv(int value, const int radix, char* buffer, const int len) {
     if (radix < 2 || radix > 16)
         return INVALID_RADIX_SPECIFIED;                         // judge radix selection
-    int rlen = value ? (ceil(log2f(value < 0 ? -value : value) / log2f(radix))
-                     + (value < 0 ? 1 : 0)) : 1;                // calculate result string length
-    if (len <= rlen)
+    int rlen = calc_rlen(value, radix);                         // calculate result string length
+    if (len < rlen)
         return BUFFER_LENGTH_OVERFLOW;                          // give error feedback
     if (value < 0)
         buffer[0] = '-', value = -value;                        // negative handle
-    for (int i = rlen; i >= 0 && value; value /= radix, i--)
+    for (int i = rlen - 2; i >= 0 && value; value /= radix, i--)
         buffer[i] = RADIX_TABLE[value % radix];                 // generate results
-    buffer[rlen + 1] = 0;                                       // make buffer C-style string
-    return rlen + 1;
+    buffer[rlen - 1] = 0;                                           // make buffer C-style string
+    return rlen;
 }
--- Project1/radix.h
+++ Project1/radix.h
@@ -5,6 +5,7 @@
 #define INVALID_RADIX_SPECIFIED (-1)
 #define BUFFER_LENGTH_OVERFLOW (-2)
 
+int calc_rlen(int value, const int radix);
 int radix_conv(int value, const int radix, char* buffer, const int len);
 
 #endif
