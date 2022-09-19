e6914cc6579c10965df9a7894e8477354d3ee119
qwqVictor <i@qwq.ren>
2022-09-19 15:06:45 +0800

    feat(proj1): main update

--- Project1/main.c
+++ Project1/main.c
@@ -1,9 +1,21 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 #include "expr.h"
+#include "radix.h"
 
 int main(int argc, char const *argv[]) {
-    char expr[80 + 1];
+    char expr[80 + 1], *result;
+    int radix;
     fgets(expr, 80, stdin);
-    printf("%d\n", eval_expr(expr, strlen(expr)));
+    int value = eval_expr(expr, strlen(expr));
+    printf("Value = %d\n", value); fflush(stdout);
+    printf("Enter radix: "); fflush(stdout);
+    scanf("%d", &radix);
+    int rlen = calc_rlen(value, radix);
+    result = (char*)malloc(rlen);
+    int ret = radix_conv(value, radix, result, rlen);
+    puts(result); fflush(stdout);
+    free((void*)result);
+    return 0;
 }
